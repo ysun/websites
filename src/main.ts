13c1d472c5970036f771d8b4d58888055bd33875
gwt805 <1973735972@qq.com>
2024-10-06 14:51:10 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import "element-plus/dist/index.css";
 import ElementPlus from "element-plus";
-// import disableDevtool from'disable-devtool';
+import disableDevtool from'disable-devtool';
 import "element-plus/theme-chalk/dark/css-vars.css";
 import zhCn from "element-plus/es/locale/lang/zh-cn";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 
 // https://github.com/theajack/disable-devtool/blob/master/README.cn.md
-// disableDevtool({url: "https://www.baidu.com/",  timeOutUrl: "https://www.baidu.com/"});
+disableDevtool({url: "https://www.baidu.com/",  timeOutUrl: "https://www.baidu.com/"});
 
 const app = createApp(App);
 
